f557c2897852041a480e579d8e34d18898a18a98
claudiaalozano <clau.lozano.perez@gmail.com>
2023-02-09 23:19:32 +0100

    creamos las dimensiones de la interface y establecemos el nombre

--- interface1.py
+++ interface1.py
@@ -1,3 +1,8 @@
 from tkinter import *
 
+root = Tk()
+root.geometry("600x600")
 
+tittle_text= Label(root, text="Prueba Interface", font=("Arial", 20))
+tittle_text.config(font=17)
+tittle_text.pack(pady=2)
