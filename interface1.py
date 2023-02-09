76ccd1e31a5e6d92490921a1033f73c8a5909873
claudiaalozano <clau.lozano.perez@gmail.com>
2023-02-09 23:24:27 +0100

    Introducimos un Spingbox para crea la casilla del input

--- interface1.py
+++ interface1.py
@@ -6,3 +6,12 @@ root.geometry("600x600")
 tittle_text= Label(root, text="Prueba Interface", font=("Arial", 20))
 tittle_text.config(font=17)
 tittle_text.pack(pady=2)
+
+main_text = Label(root, text = "Por favor introduzca el numero de zonas")
+main_text.pack(pady=80)
+main_text.config(font=15)
+
+s = Spinbox(root, from_=0, to_=100000, increment=1)
+s.pack(pady=10)
+s.config()
+
