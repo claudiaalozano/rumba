232befbc672781fc81b5e08472d606ce8e9aa0ae
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2023-03-06 16:20:56 +0100

    lo raneamos

--- menu.py
+++ menu.py
@@ -1,1 +1,18 @@
 from roomba import *
+
+def MenuPrincipal():
+
+    tittle_text= Label(root, text="Prueba Interface", font=("Arial", 20))
+    tittle_text.config(font=17)
+    tittle_text.pack(pady=2)
+
+    main_text = Label(root, text = "Por favor introduzca el numero de zonas")
+    main_text.pack(pady=80)
+    main_text.config(font=15)
+
+    s = Spinbox(root, from_=0, to_=100000, increment=1)
+    s.pack(pady=10)
+    s.config()
+
+    buttom = Button(root, text="Aceptar", command=lambda: calculos(s.get()))
+    buttom.pack(pady=10)
--- roomba.py
+++ roomba.py
@@ -1,22 +1,7 @@
 from tkinter import *
 root = Tk()
 root.geometry("600x600")
-def MenuPrincipal():
 
-    tittle_text= Label(root, text="Prueba Interface", font=("Arial", 20))
-    tittle_text.config(font=17)
-    tittle_text.pack(pady=2)
-
-    main_text = Label(root, text = "Por favor introduzca el numero de zonas")
-    main_text.pack(pady=80)
-    main_text.config(font=15)
-
-    s = Spinbox(root, from_=0, to_=100000, increment=1)
-    s.pack(pady=10)
-    s.config()
-
-    buttom = Button(root, text="Aceptar", command=lambda: calculos(s.get()))
-    buttom.pack(pady=10)
 
 def calculos(n):
     n= int(n)
@@ -88,7 +73,5 @@ def check(zonas):
             return False
     return True
 
-
-MenuPrincipal()
 
 root.mainloop()
